21c47130ef2151c965aa3ca93569af4432f1fba5
Frank Schroeder <frschroeder@ebay.com>
2015-11-28 00:07:28 +0100

    Move Target to target.go so that I stop looking for it

--- route/route.go
+++ route/route.go
@@ -39,27 +39,6 @@ type route struct {
 	total uint64
 }
 
-type Target struct {
-	// service is the name of the service the targetURL points to
-	service string
-
-	// tags are the list of tags for this target
-	tags []string
-
-	// URL is the endpoint the service instance listens on
-	URL *url.URL
-
-	// fixedWeight is the weight assigned to this target.
-	// If the value is 0 the targets weight is dynamic.
-	fixedWeight float64
-
-	// weight is the actual weight for this service in percent.
-	weight float64
-
-	// timer measures throughput and latency of this target
-	Timer gometrics.Timer
-}
-
 func newRoute(host, path string) *route {
 	return &route{host: host, path: path}
 }
