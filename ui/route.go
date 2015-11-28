63ef0d1f91744b3867259ccd7cef03f383bd55e0
Frank Schroeder <frschroeder@ebay.com>
2015-11-28 01:09:23 +0100

    Issue #16: Improve filter on routing table ui
    * Filter only on routes
    * Remember filter value

--- ui/route.go
+++ ui/route.go
@@ -3,16 +3,27 @@ package ui
 import (
 	"html/template"
 	"net/http"
+	"strings"
 
 	"github.com/eBay/fabio/route"
 )
 
 func handleRoute(w http.ResponseWriter, r *http.Request) {
+	var cfg [][]string
+	for _, s := range route.GetTable().Config(true) {
+		p := strings.Split(s, "tags")
+		if len(p) == 1 {
+			cfg = append(cfg, []string{s, ""})
+		} else {
+			cfg = append(cfg, []string{strings.TrimSpace(p[0]), "tags" + p[1]})
+		}
+	}
+
 	data := struct {
-		Config    []string
+		Config    [][]string
 		ConfigURL string
 	}{
-		route.GetTable().Config(true),
+		cfg,
 		configURL,
 	}
 	tmplTable.ExecuteTemplate(w, "table", data)
@@ -28,29 +39,45 @@ var htmlTable = `
   <title>fabio routing table</title>
   <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
   <style type="text/css">
-  	pre, input { font-size: 14px; }
+    *, html, body {font-family: sans-serif;}
+  	table, tr, td {text-align: left;}
+  	td, input {font-family: monospace; font-size: 12px;}
   </style>
 </head>
 <body>
-<h1>Routing Table</h1>
+<h2>./fabio - Routing Table</h2>
 
 <p>Filter routes: <input type="text" id="filter"></p>
 
+<table>
+<tbody>
 {{range $i, $v := .Config}}
-<pre>{{$v}}</pre>
+	<tr><td class="route">{{index $v 0}}</td><td>{{index $v 1}}</td></tr>
 {{end}}
+</tbody>
+</table>
 
 <p><a href="{{.ConfigURL}}" target="_new">Edit config</a></p>
 
 <script>
-$filter = $('#filter');
-$filter.focus();
-$filter.keyup(function() {
-	$("pre").show();
-	if ($filter.val() == '') {
-		return;
+
+$(function(){
+	var $filter = $('#filter');
+	var params={};window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(str,key,value){params[key] = value;});
+	if (params.filter) {
+		$filter.val(params.filter);
+		$("td.route:not(:contains('"+params.filter+"'))").each(function() {
+			$(this).parent("tr").hide();
+		});
 	}
-	$("pre:not(:contains('"+$filter.val()+"'))").hide();
+	$filter.focus();
+	$filter.keyup(function() {
+		var url = window.location.href.split('?')[0];
+		if ($filter.val() != '') {
+			url +=  "?filter=" +$filter.val();
+		}
+		window.location = url;
+	})
 })
 </script>
 
