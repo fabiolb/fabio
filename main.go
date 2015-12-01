bfc0548cc0477984f9b7453de2769c2ffbca25cf
Frank Schroeder <frschroeder@ebay.com>
2015-12-01 11:58:04 +0100

    Release v1.0.6
    Fixes #9: websocket support
    Fixes #15: Traffic shaping does not match on service name
    Fixes #16: Filter routing table not on tags
    Fixes #18: Manage manual overrides via UI

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 // It is also set by the linker when fabio
 // is built via the Makefile or the build/docker.sh
 // script to ensure the correct version nubmer
-var version = "1.0.6-dev"
+var version = "1.0.6"
 
 func main() {
 	var filename string
