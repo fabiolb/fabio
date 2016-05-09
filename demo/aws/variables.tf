ea4831df62e61bcb3cfeb39e991ab5c15e1790d9
Frank Schroeder <frschroeder@ebay.com>
2016-05-09 22:42:05 +0200

    Add Terraform config for AWS test

---
+++ demo/aws/variables.tf
@@ -0,0 +1,19 @@
+variable "public_key_path" {
+  default = "~/.ssh/terraform.pub"
+}
+
+variable "key_name" {
+  description = "terraform"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default = "eu-west-1"
+}
+
+# Ubuntu Precise 14.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-west-1 = "ami-f95ef58a"
+  }
+}
