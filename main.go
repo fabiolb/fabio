85ac7bebca6bbcf71c81c721409f26b73268302f
Frank Schroeder <frschroeder@ebay.com>
2016-10-12 10:29:42 +0200

    Release v1.3.3

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 // It is also set by the linker when fabio
 // is built via the Makefile or the build/docker.sh
 // script to ensure the correct version nubmer
-var version = "1.3.2"
+var version = "1.3.3"
 
 func main() {
 	cfg, err := config.Load()
