ee1edf5eadef40039f9697de739986c4f99ef845
maximka777 <maksim.borovskij@yandex.ru>
2018-12-14 19:13:43 +0300

    Untagged metrics abstraction

---
+++ metrics4/untagged_test.go
@@ -0,0 +1,23 @@
+package metrics4
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseLabelsValues(t *testing.T) {
+	labelsNames := []string{ "a", "b" }
+	labels := []string { "a", "1", "b", "2" }
+	labelsValues, err := parseLabelsValues(labelsNames, labels)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.EqualValues(t, []string{ "1", "2" }, labelsValues)
+}
+
+func TestMakeNameFromLabels(t *testing.T) {
+	labelsNames := []string{ "a", "b" }
+	labels := []string { "a", "1", "b", "2" }
+	name := makeNameFromLabels(labelsNames, labels)
+	assert.EqualValues(t, "a_1_b_2", name)
+}
