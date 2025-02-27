f2d5e15d01f8550a81fc69c20812b514165995b5
Tristan Morgan <tristan.morgan@hashicorp.com>
2025-02-27 13:11:22 +1100

    Remove Tracing and update packages even more. (#970)

--- proxy/http_proxy.go
+++ proxy/http_proxy.go
@@ -19,7 +19,6 @@ import (
 	"github.com/fabiolb/fabio/noroute"
 	"github.com/fabiolb/fabio/proxy/gzip"
 	"github.com/fabiolb/fabio/route"
-	"github.com/fabiolb/fabio/trace"
 	"github.com/fabiolb/fabio/uuid"
 )
 
@@ -67,9 +66,6 @@ type HTTPProxy struct {
 	// Logger is the access logger for the requests.
 	Logger logger.Logger
 
-	// TracerCfg is the Open Tracing configuration as provided during startup
-	TracerCfg config.Tracing
-
 	// UUID returns a unique id in uuid format.
 	// If UUID is nil, uuid.NewUUID() is used.
 	UUID func() string
@@ -93,10 +89,6 @@ func (p *HTTPProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		r.Header.Set(p.Config.RequestID, id())
 	}
-
-	//Create Span
-	span := trace.CreateSpan(r, &p.TracerCfg)
-	defer span.Finish()
 
 	t := p.Lookup(r)
 
@@ -186,9 +178,6 @@ func (p *HTTPProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	addResponseHeaders(w, r, p.Config)
-
-	//Add OpenTrace Headers to response
-	trace.InjectHeaders(span, r)
 
 	upgrade, accept := r.Header.Get("Upgrade"), r.Header.Get("Accept")
 
