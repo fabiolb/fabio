a4ad9cb10c27cc935fb8e8b5f5c8f08b6ea6a614
Tristan Morgan <tristan.morgan@hashicorp.com>
2025-02-27 13:11:22 +1100

    Remove Tracing and update packages even more. (#970)

--- route/issue57_test.go
+++ route/issue57_test.go
@@ -33,7 +33,7 @@ func TestIssue57(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%d: got %v want nil", i, err)
 		}
-		target := tbl.Lookup(req, "", rrPicker, prefixMatcher, globCache, globEnabled)
+		target := tbl.Lookup(req, rrPicker, prefixMatcher, globCache, globEnabled)
 		if target == nil {
 			t.Fatalf("%d: got %v want %v", i, target, want)
 		}
