3458c0c527e0943f7c864c09716a960b236f96d2
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2025-07-25 12:40:47 +1000

    Links to homepage.

--- admin/ui/manual.go
+++ admin/ui/manual.go
@@ -65,6 +65,7 @@ var tmplManual = template.Must(template.New("manual").Funcs(funcs).Parse( // lan
                 <li><a class="dropdown-trigger dropdown-button" href="#" data-target="overrides">Overrides<i class="material-icons right">arrow_drop_down</i></a></li>
 				<li><a href="https://github.com/fabiolb/fabio/blob/master/CHANGELOG.md">{{.Version}}</a></li>
 				<li><a href="https://github.com/fabiolb/fabio">Github</a></li>
+				<li><a href="https://fabiolb.net">Fabiolb.net</a></li>
 			</ul>
 		</div>
 	</div>
