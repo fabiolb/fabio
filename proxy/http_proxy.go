b2f9d6348be34281dbf0467d6e08d7d43f0cd0c5
fatelei <fatelei@gmail.com>
2025-09-23 20:11:10 +0800

    chore: Remove outdated tracing functions

--- proxy/http_proxy.go
+++ proxy/http_proxy.go
@@ -19,7 +19,6 @@ import (
 	"github.com/fabiolb/fabio/noroute"
 	"github.com/fabiolb/fabio/proxy/gzip"
 	"github.com/fabiolb/fabio/route"
-	"github.com/fabiolb/fabio/trace"
 	"github.com/fabiolb/fabio/uuid"
 )
 
@@ -67,8 +66,6 @@ type HTTPProxy struct {
 	// Logger is the access logger for the requests.
 	Logger logger.Logger
 
-	// TracerCfg is the Open Tracing configuration as provided during startup
-	TracerCfg config.Tracing
 
 	// UUID returns a unique id in uuid format.
 	// If UUID is nil, uuid.NewUUID() is used.
@@ -94,9 +91,6 @@ func (p *HTTPProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.Header.Set(p.Config.RequestID, id())
 	}
 
-	//Create Span
-	span := trace.CreateSpan(r, &p.TracerCfg)
-	defer span.Finish()
 
 	t := p.Lookup(r)
 
@@ -187,8 +181,6 @@ func (p *HTTPProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	addResponseHeaders(w, r, p.Config)
 
-	//Add OpenTrace Headers to response
-	trace.InjectHeaders(span, r)
 
 	upgrade, accept := r.Header.Get("Upgrade"), r.Header.Get("Accept")
 
